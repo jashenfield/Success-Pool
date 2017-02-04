c4e13488052ff4a3c0f2d4a78b3220a9e888aa41
jashenfield <j.shenfield0@gmail.com>
2017-02-04 00:04:16 -0800

    Wirefram and Surface
    Added wireframe and surface plots showing the results, plus bugfixes
    (was mapping the sucesses to result matrix incorrectly)

--- dice_pool.py
+++ dice_pool.py
@@ -1,41 +1,98 @@
 import numpy as np
 import operator as op
 import functools
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
+
+
+######################
+# FUNCTIONS FOR MATH #
+######################
+
+# Function to caluculate nCr that I found on StackExchange
+# Credit to user dheerosaur
 def ncr(n, r):
     r = min(r, n-r)
     if r == 0: return 1
-    numer = functools.reduce(op.mul, range(n, n-r, -1))
-    denom = functools.reduce(op.mul, range(1, r+1))
+    numer = functools.reduce(op.mul, np.arange(n, n-r, -1))
+    denom = functools.reduce(op.mul, np.arange(1, r+1))
     return numer//denom
 
-# calculate the chance to reach the target number of hits
+# Calculate the chance to reach the target number of hits
 def chance(num, hits, target = 4):
-    sum_chance = 0
+    sum_chance = 0  # Bootleg Sigma
     fail = (target - 1) / 6
-    for k in range(hits, num+1):
+    for k in np.arange(hits, num+1):
         sum_chance += ncr(num, k) * pow(fail,k) * pow((1-fail), (num-k))
     return sum_chance
 
-pool = np.arange(1, 11)
-targetnum = np.arange(1,11)
 
-fig = plt.figure()
-ax1 = fig.add_subplot(111, projection='3d')
 
-xpos = [1,2,3,4,5,6,7,8,9,10]
-ypos = [2,3,4,5,1,6,2,1,7,2]
-num_elements = len(xpos)
-zpos = [0,0,0,0,0,0,0,0,0,0]
-dx = np.ones(10)
-dy = np.ones(10)
-dz = [1,2,3,4,5,6,7,8,9,10]
 
-ypos2 = [4,1,5,2,7,3,8,9,1,6]
-dz2 = [6,7,8,9,10,1,2,3,4,5]
+#############################
+# SETTING THE DATA MATRICES #
+#############################
 
-ax1.bar3d(xpos, ypos, zpos, dx, dy, dz, color='#00ceaa')
-ax1.bar3d(xpos, ypos2, zpos, dx, dy, dz2, color='#ff0000')
+# X = the size of the dice pool
+# Y = the number of sucesses needed
+x = y = np.arange(1, 11)
+X, Y = np.meshgrid(x, y)      # Setting up matrices of X and Y values
+Z = np.zeros((10, 10))        # Set up a matrix of zeroes to alter later
+
+for i in x:
+    for j in y:
+        if i >= j:
+            prob = chance(x[i-1], y[j-1])
+            # Need to switch x and y coordinates
+            # That's just how it works
+            # Took me forever to figure it out
+            Z[j-1][i-1] = prob
+
+
+
+
+
+
+########################
+# SETTING UP THE PLOTS #
+########################
+
+
+#makes figure twice as tall as wide
+fig = plt.figure(figsize=plt.figaspect(2.))
+
+# wire subplot
+ax = fig.add_subplot(2, 1, 1, projection='3d')
+wire = ax.plot_wireframe(X,Y,Z, rstride=1, cstride=1)
+ax.set_xlabel('Pool Size')
+ax.set_ylabel('Target Number')
+ax.set_zlabel('Chance of Success')
+
+#surface subplot
+
+ax = fig.add_subplot(2, 1, 2, projection='3d')
+surf = ax.plot_surface(X, Y, Z, linewidth=0, cstride=1,
+                       rstride=1, cmap=cm.coolwarm,
+                       antialiased=False)
+ax.set_xlabel('Pool Size')
+ax.set_ylabel('Target Number')
+ax.set_zlabel('Chance of Success')
+
+# color bar for the surface plot
+fig.colorbar(surf, shrink=1, aspect=5)
+
+
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
